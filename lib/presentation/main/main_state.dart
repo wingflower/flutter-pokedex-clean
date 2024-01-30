e334ea918dd74cfd81c87376333d4b50811ec659
dodocap <dodocap@naver.com>
2024-01-30 11:30:43 +0900

    Feat: 메인화면 FireStore 연동하여 컬렉션 표시
    - FireStore 관련 UseCase의 필드명(Repository) 구체적으로 변경
    - DI 코드 포맷팅
    - 그리드뷰 isCollected 기본값 설정 삭제

--- lib/presentation/main/main_state.dart
+++ lib/presentation/main/main_state.dart
@@ -1,14 +1,18 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:pokedex_clean/domain/model/pokemon.dart';
+import 'package:pokedex_clean/domain/model/user_info.dart';
 
 part 'main_state.freezed.dart';
 
 @freezed
 class MainState with _$MainState {
   const factory MainState({
+    @Default('') String email,
+    @Default(UserInfo(pokemons: [])) UserInfo userInfo,
     @Default(false) bool isLoading,
     @Default(0) int rewardTime,
     @Default([]) List<Pokemon> pokemonListData,
     @Default(2) int gridCrossAxisCount,
+    @Default(-1) int getPokemonIndex,
   }) = _MainState;
 }
