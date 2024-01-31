ef4a76620ca5784ac1ddff7fd03a54208080c630
sc2bat <izar_dero@naver.com>
2024-01-31 11:18:06 +0900

    Fix: di_setup 누락된 코드 추가

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -12,8 +12,8 @@ import 'package:pokedex_clean/domain/repository/pokemon_repository.dart';
 import 'package:pokedex_clean/domain/repository/user_account_repository.dart';
 import 'package:pokedex_clean/domain/repository/user_info_repository.dart';
 import 'package:pokedex_clean/domain/use_case/collection/get_pokemon_use_case.dart';
-import 'package:pokedex_clean/domain/use_case/collection/sort_pokemon_list_use_case.dart';
 import 'package:pokedex_clean/domain/use_case/collection/search_by_name_pokemon_use_case.dart';
+import 'package:pokedex_clean/domain/use_case/collection/sort_pokemon_list_use_case.dart';
 import 'package:pokedex_clean/domain/use_case/info/add_and_update_user_info_use_case.dart';
 import 'package:pokedex_clean/domain/use_case/info/get_user_info_use_case.dart';
 import 'package:pokedex_clean/domain/use_case/user/check_verify_use_case.dart';
@@ -138,6 +138,7 @@ void diSetup() {
     SortedPokemonListUseCase(
       pokemonRepository: getIt<PokemonRepository>(),
     ),
+  );
   getIt.registerSingleton<SearchByNamePokemonUseCase>(
     SearchByNamePokemonUseCase(),
   );
